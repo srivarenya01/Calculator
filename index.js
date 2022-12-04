ced6e4393ad04a40e8d2a9b3aecc7417750b2b36
Mudumba Sri Varenya <srivarenya123@gmail.com>
2022-12-05 01:06:14 +0530

    Completed styles

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var lastOp = false;
 
-
+// Function to add event Lisners
 function addEventToClass(arr, event, func){
     for(let i=0;i<arr.length;i++){
         arr[i].addEventListener(event, func);
@@ -10,6 +10,7 @@ function addEventToClass(arr, event, func){
 let keyBlock = document.getElementsByClassName("calcCols");
 addEventToClass(keyBlock, "click", calcClick);
 
+// To Catch key press from Keyboard
 document.addEventListener('keydown', (event)=>{
     var num = event.key;
     click = new Event("click");
@@ -35,7 +36,7 @@ document.addEventListener('keydown', (event)=>{
     }
 }, false);
 
-
+// Calculation of answer
 function check(val){
     let pres = document.getElementById("out");
     let present = pres.innerHTML;
@@ -71,7 +72,7 @@ function check(val){
                 } catch (error) {
                     present = "ERROR";
                 }
-                if(present == "undefined"){
+                if(present == "undefined" || present == "NaN"){
                     present = "ERROR";
                 }else{
                     let histele = document.createElement("div");
@@ -97,6 +98,7 @@ function check(val){
             let k = present[present.length-1];
             if(!spl.includes(k)){
                 present = parseFloat(String(Math.sqrt(Number(present)))).toPrecision(6);
+                present = String(Number(present))
                 if(present == "NaN"){
                     present = "ERROR";
                 }else{
@@ -120,12 +122,21 @@ function check(val){
     pres.innerHTML = present;
 }
 
+// Catching clicks on keypad
 function calcClick(){
     let clickedon = this.childNodes[1].id;
-    check(clickedon);
+    animate(clickedon)
     toggle(1);
 }
 
+function animate(k){
+    let j = document.getElementById(k)
+    $(j).fadeTo("fast", 0.3);
+    $(j).fadeTo("fast", 1);
+    check(k);
+}
+
+// Catching the selection of history
 let hist = document.getElementsByClassName("hist")[0];
 hist.addEventListener("click", () => {
     let gjs = $(".history");
@@ -136,6 +147,7 @@ hist.addEventListener("click", () => {
     }
 })
 
+// Changing the history visibilities on click
 function toggle(k){
     let gjs = $(".history");
     let i = document.getElementsByTagName("i")[0];
